a775850f38c03f233396985aa2ec1108fcc9a14a
wanna-be-pro <work.mahmudulhasan@gmail.com>
2022-12-11 00:26:44 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is a blog site. </h1>
+      <p>you can read or post blogs from this site.</p>
     </div>
   );
 }
